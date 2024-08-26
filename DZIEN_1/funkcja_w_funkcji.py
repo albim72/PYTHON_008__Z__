30339106c8dd55d23b1e24fa3ad80b5424c8a420
albim72 <marcin.albiniak72@gmail.com>
2024-08-26 11:09:33 +0200

    Update funkcja_w_funkcji.py

--- DZIEN_1/funkcja_w_funkcji.py
+++ DZIEN_1/funkcja_w_funkcji.py
@@ -1,3 +1,4 @@
+#przykład 2
 def rejestracja(oplata):
     def lista(nr):
         return f'jesteś na liście zawodników pod numerem: {nr}'
@@ -16,3 +17,25 @@ def rejestracja(oplata):
 print(rejestracja(1)(645))
 print(rejestracja(0)())
 print(rejestracja(453)())
+
+#przykład 3 -> prosty dekorator
+
+def startstop(funkcja):
+    def wrapper(*args):
+        print("*"*50)
+        print("startowanie procesu...")
+        funkcja(*args)
+        print("kończenie procesu...")
+    return wrapper
+
+def zawijanie(w_co):
+    print(f"zawijanie czekoladek w {w_co}")
+
+zw = startstop(zawijanie)
+zw("sreberka")
+
+@startstop
+def dmuchanie(czego):
+    print(f'dmuchanie {czego} na urodziny!')
+
+dmuchanie("baloników")
