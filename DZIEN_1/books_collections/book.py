fdc685e4b8e5f498a6c6e9b3b99cba99804ae8c5
albim72 <marcin.albiniak72@gmail.com>
2024-08-26 16:01:14 +0200

    Update book.py

--- DZIEN_1/books_collections/book.py
+++ DZIEN_1/books_collections/book.py
@@ -6,11 +6,18 @@ class Book(ABC):
         self.author = author
         self.year = year
         self.pages = pages
-        
+
+    def __repr__(self):
+        return f"obiekt klasy: {self.__class__.__name__} ({self.title}-{self.author})"
+
+    def __call__(self,price):
+        return f'ilosc stron z dodatkami: {self.pages + 5}, cena:{price}'
+
     @abstractmethod
     def get_info(self):
         pass
-    
+
     @abstractmethod
     def read(self):
         pass
+
