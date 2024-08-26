4793f4fece9fa24442e15eef86c8873ecc73f867
albim72 <marcin.albiniak72@gmail.com>
2024-08-26 15:27:16 +0200

    Update main.py

--- DZIEN_1/moje_moduly/main.py
+++ DZIEN_1/moje_moduly/main.py
@@ -3,5 +3,20 @@
 from dane import nrfilii as nf
 from dane import book as bk
 
+from funkcje.bfunkcje import czytaj_liste,czytaj_slownik
+from klasy.classdata import CDane
+
+print("______________ wyswietlenie bezpośrednie ________________")
 print(nf)
 print(bk)
+
+print("______________ wyswietlenie za pomocą funkcji ________________")
+czytaj_liste(nf)
+czytaj_slownik(bk)
+
+print("______________ wyswietlenie za pomocą modelu ________________")
+
+cd = CDane(nf,bk)
+cd.czytaj_l()
+cd.czytaj_s()
+
