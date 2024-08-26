a625a00d279cf0d3a919a2c0540be0e1e00e670e
albim72 <marcin.albiniak72@gmail.com>
2024-08-26 15:45:06 +0200

    Create bcollections.py

---
+++ DZIEN_1/books_collections/bcollections.py
@@ -0,0 +1,15 @@
+from book import Book
+
+class BookCollections:
+    def __init__(self):
+        self.books = []
+        
+    def add_book(self,book:Book):
+        self.books.append(book)
+        
+    def rwmove_book(self,book:Book):
+        self.books.remove(book)
+        
+    def display_all_books(self):
+        for book in self.books:
+            print(book.get_info())
