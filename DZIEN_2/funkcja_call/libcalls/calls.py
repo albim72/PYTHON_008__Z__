240bb5ecbff55e038ae4d4c17671c879dd907c15
albim72 <marcin.albiniak72@gmail.com>
2024-08-27 09:13:30 +0200

    Create calls.py

---
+++ DZIEN_2/funkcja_call/libcalls/calls.py
@@ -0,0 +1,10 @@
+#przypadek 1 - licznik wywołań funkcji
+
+class CallCounter:
+    def __init__(self):
+        self.count = 0
+
+    def __call__(self, *args, **kwargs):
+        self.count += 1
+        print(f"funkcję call wywołano {self.count} razy")
+        
