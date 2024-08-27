95de19dfd96d048f938b029830563cc9f964a25d
albim72 <marcin.albiniak72@gmail.com>
2024-08-27 10:10:11 +0200

    Update slownik_args.py

--- DZIEN_2/slownik_args.py
+++ DZIEN_2/slownik_args.py
@@ -33,3 +33,40 @@ def user_datails(**kwargs):
 
 user_datails(name="Alicja", age=25, location="Sopot", profession="inżynier sieci")
 user_datails(name="Leon",age=23, location="Opole")
+
+#przykład 3- dekroatory przyjmujące argumenty
+
+def logging_decorator(func):
+    def wrapper(*args,**kwargs):
+        print(f"Wołana funckja: {func.__name__} z argumentami {args} i argumentami słownikowymi: {kwargs}")
+        return func(*args,**kwargs)
+    return wrapper
+
+@logging_decorator
+def add(a,b,**kwargs):
+    result =  a*2+b*3
+    for key,value in kwargs.items():
+        print(f"procesowanie dodatkowego argumentu {key} z wartością: {value}")
+        if key == 'add_to_result':
+            result += value
+        elif key == 'multiply_result':
+            result *= value
+    return result
+
+result = add(2,3)
+print(result)
+
+result_with_kwargs = add(2,3,add_to_result = 7)
+print(result_with_kwargs)
+
+result_multiply = add(2,3,add_to_result=7,multiply_result=2)
+print(result_multiply)
+
+result_m2 = add(2,3,multiply_result=2)
+print(result_m2)
+
+result_x = add(2,3,blabla=12)
+print(result_x)
+
+result_multiply_2 = add(2,3,multiply_result=2,add_to_result=7)
+print(result_multiply_2)
