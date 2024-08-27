271493efd0e367186cedf52612fd3f508589e291
albim72 <marcin.albiniak72@gmail.com>
2024-08-27 13:53:45 +0200

    Update figura.py

--- DZIEN_2/figury/figura.py
+++ DZIEN_2/figury/figura.py
@@ -1,9 +1,14 @@
 from abc import ABC,abstractmethod
 
 class Figura(ABC):
-    def __init__(self,a,b):
+    # def __init__(self,a,b):
+    #     self.a = a
+    #     self.b = b
+
+    def __init__(self,a,b=None):
         self.a = a
-        self.b = b
+        if b:
+            self.b = b
 
     @abstractmethod
     def policz_pole(self):
