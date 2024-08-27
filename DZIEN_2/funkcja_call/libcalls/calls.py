925d3e41c75e889a8dcadc1cf3aeb3ba044cf78a
albim72 <marcin.albiniak72@gmail.com>
2024-08-27 09:20:49 +0200

    Update calls.py

--- DZIEN_2/funkcja_call/libcalls/calls.py
+++ DZIEN_2/funkcja_call/libcalls/calls.py
@@ -7,4 +7,18 @@ class CallCounter:
     def __call__(self, *args, **kwargs):
         self.count += 1
         print(f"funkcję call wywołano {self.count} razy")
-        
+
+#przypadek 2 - walidacja danych wejściowych
+
+class RangeValidator:
+    def __init__(self,min_value,max_value):
+        self.min_value = min_value
+        self.max_value = max_value
+
+    def __call__(self, value):
+        if self.min_value <= value <= self.max_value:
+            print(f'wartośc {value} mieści się w przedziale (min:{self.min_value},max:{self.max_value})')
+            return True
+        else:
+            print(f'wartośc {value} jest poza przedziałem (min:{self.min_value},max:{self.max_value})')
+            return True
