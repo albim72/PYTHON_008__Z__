9ed3d8620b48b7de27ca1e58112b45da19764e22
albim72 <marcin.albiniak72@gmail.com>
2024-08-27 09:36:18 +0200

    Update main.py

--- DZIEN_2/funkcja_call/main.py
+++ DZIEN_2/funkcja_call/main.py
@@ -1,4 +1,4 @@
-from libcalls.calls import CallCounter,RangeValidator
+from libcalls.calls import CallCounter,RangeValidator,CacheFunction
 
 #prezentacja licznika wywołań funkcji
 counter = CallCounter()
@@ -13,3 +13,19 @@ valid = RangeValidator(2,16)
 valid(3)
 valid(2)
 valid(19)
+
+#prezentacja obsługi cache
+
+def efunction(x,y):
+    return x**y + y**x
+
+#obiekt CacheFunction
+ce_func = CacheFunction(efunction)
+
+print(ce_func(2,3))
+print(ce_func(2,2))
+print(ce_func(2,3))
+print(ce_func(13,1))
+print(ce_func(13,3))
+print(ce_func(13,3))
+print(ce_func(2,3))
