23fb6b6e4b134d1ba559770cf9f6b77f1e231185
albim72 <marcin.albiniak72@gmail.com>
2024-08-27 13:54:21 +0200

    Create kolo.py

---
+++ DZIEN_2/figury/kolo.py
@@ -0,0 +1,13 @@
+import math
+from figura import Figura
+
+class Kolo(Figura):
+    # def __init__(self, a):
+    #     super().__init__(a, 0)
+
+    def __init__(self, a):
+        super().__init__(a)
+
+    def policz_pole(self):
+        return math.pi*self.a**2
+
