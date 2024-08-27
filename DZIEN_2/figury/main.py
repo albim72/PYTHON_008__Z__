c53de130e68d3ee02b546febf776689003c40598
albim72 <marcin.albiniak72@gmail.com>
2024-08-27 13:54:37 +0200

    Update main.py

--- DZIEN_2/figury/main.py
+++ DZIEN_2/figury/main.py
@@ -1,6 +1,7 @@
 from trojkat import Trojkat
 from prostokat import Prostokat
 from trapez import Trapez
+from kolo import Kolo
 
 tr = Trojkat(5.6,8.7)
 print(f'pole figury {tr.__class__.__name__} wynosi {tr.policz_pole():.2f} cm2')
@@ -18,3 +19,11 @@ print('_'*50)
 trp = Trapez(8.3,5.8,4.6)
 print(f'pole figury {trp.__class__.__name__} wynosi {trp.policz_pole():.2f} cm2')
 print('_'*50)
+
+kl = Kolo(5.5)
+print(f'pole figury {kl.__class__.__name__} wynosi {kl.policz_pole():.2f} cm2')
+# print(kl.b)
+# kl.b =188
+# print(kl.b)
+print('_'*50)
+
