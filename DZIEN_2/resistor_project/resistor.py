e811e480af91e971e9c9acc12e815d6c582ec102
albim72 <marcin.albiniak72@gmail.com>
2024-08-27 10:29:54 +0200

    Update resistor.py

--- DZIEN_2/resistor_project/resistor.py
+++ DZIEN_2/resistor_project/resistor.py
@@ -3,4 +3,10 @@ class Resistor:
         self.ohms = ohms
         self.voltage = 0
         self.current = 0
-        
+
+    def __repr__(self):
+        return f"obiekt klasy {self.__class__.__name__}"
+
+    def __str__(self):
+        return f"komunikat tekstowy - opisujący obiekt klasy {self.__class__.__name__}"
+
