1312c7e1989c21c508160ae342af5d65ad6e938e
albim72 <marcin.albiniak72@gmail.com>
2024-08-28 13:15:23 +0200

    Update filozof.py

--- DZIEN_3/filozof.py
+++ DZIEN_3/filozof.py
@@ -40,6 +40,9 @@ class Kopernik(metaclass=SednoOdpowiedzi):
     #     return "Nie!Ziemia jest elipsoidą!"
     pass
 
+class Einstein(metaclass=SednoOdpowiedzi):
+    pass
+
 fil1 = Arystoteles()
 print(f'Filozof {fil1.__class__.__name__} twierdzi: {fil1.odpowiedz()}')
 
@@ -52,6 +55,12 @@ print(f'Filozof {fil3.__class__.__name__} twierdzi: {fil3.odpowiedz()}')
 fil4 = Kopernik()
 print(f'Filozof {fil4.__class__.__name__} twierdzi: {fil4.odpowiedz()}')
 
+fil5 = Einstein()
+print(f'Filozof {fil5.__class__.__name__} twierdzi: {fil5.odpowiedz()}')
+
 #zadanie 1 -> skonstruuj rozwiązanie pozwalające Kopernikowi na wypowiedź: Nie! Ziemia jest elipsoidą!
 #pozostaw aktualną konstrukcję klas (klasa Kopernik oparta ma byc na metaklasie SednoOdpowiedzi
+
+#zadanie 2 -> przebuduj rozwiązanie w ten sposób aby można dodac dowolną ilosc klas opisujących filozofów nowożytnych
+#którzy będą odpowiadac tak jak Kopernik
 
