702f4ec9f8c4c234efef4aae58ce092b3ae11e74
albim72 <marcin.albiniak72@gmail.com>
2024-08-29 11:29:10 +0200

    Create iterator.py

---
+++ WZORCE_PROJEKTOWE/iterator.py
@@ -0,0 +1,39 @@
+class FootballTeamIterator:
+    def __init__(self,members):
+        self.members = members
+        self.index=0
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.index<len(self.members):
+            val = self.members[self.index]
+            self.index +=1
+            return val
+        else:
+            raise StopIteration()
+
+class FootballTeam:
+    def __init__(self,members):
+        self.members = members
+
+    def __iter__(self):
+        return FootballTeamIterator(self.members)
+
+def main():
+    members = [f'gracz {str(x)}' for x in range(1,23)]
+    members = members + ["trener1","trener2","trener3","fizjoterapeuta","lekarz","psycholog","rzecznik"]
+    team = FootballTeam(members)
+    team_it = iter(team)
+
+    # if member in team_it:
+    #     print(member)
+    try:
+        while True:
+            print(next(team_it))
+    except StopIteration as si:
+        print(si)
+
+if __name__ == '__main__':
+    main()
