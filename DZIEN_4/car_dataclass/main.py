06183d3f951b9535dc6f31b9a9f1eeed86790f64
albim72 <marcin.albiniak72@gmail.com>
2024-08-29 09:52:14 +0200

    Update main.py

--- DZIEN_4/car_dataclass/main.py
+++ DZIEN_4/car_dataclass/main.py
@@ -1,6 +1,22 @@
 from car import Car
+from inventory import CarInventory
 
-# Przykładowe użycie
-car = Car(mark="Tesla", model="Model S", year=2022, price=79999.99, is_electric=True)
+car1 = Car(mark="Tesla", model="Model S", year=2022, price=270000, is_electric=True)
+car2 = Car(mark="Toyota", model="Avensis",year=2016,price=45000,is_electric=False)
+car3 = Car(mark="BMW", model="i3",year=2021,price=112000,is_electric=True)
 
-print(car.car_description())
+#tworzenie inwentarza
+invent = CarInventory()
+invent.add_car(car1)
+invent.add_car(car2)
+invent.add_car(car3)
+
+invent.list_inventory()
+
+print("\nSamochody marki Tesla:")
+tesla_ = invent.find_cars_by_mark("Tesla")
+for car in tesla_:
+    print(car.car_description())
+
+invent.remove_car(car2)
+invent.list_inventory()
